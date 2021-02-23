12670695d6665ebb6ea61a8dbfb11a762e2502f8
Charlesccharris <harriscc@student.swosu.edu>
2021-02-23 15:30:34 -0600

    Just removed the stuff that wasn't being used

--- PrimeGetter/Main.java
+++ PrimeGetter/Main.java
@@ -1,34 +1,96 @@
 import java.util.Scanner;
-import java.math.*;
+import java.util.*;
 
-public class Main{
+class Main{
         public static void main(String[] args){
-                /*(Large prime numbers)
-                Write a program that finds five prime numbers
-                larger than Long.MAX_VALUE.*/
+
+                int[] prime = new int[100];
                 Scanner input = new Scanner(System.in);
+                StackOfIntegers listOfPrimes = new StackOfIntegers();
+                int number = 1;
+                int newNumber = 0;
+                int size = 1;
 
-                boolean isPrime = true;
-                BigInteger ourNumber = new BigInteger("9871284");
-                int totalNumbers = 5;
-                int foundNumbers = 0;
+                System.out.print("Please input your whole number: ");
+                number = input.nextInt();
+		prime = getPrimes(number, prime);
 
-                while(foundNumbers < totalNumbers){
-                        isPrime = true;
-                        for(BigInteger primeChecker = BigInteger.valueOf(2); primeChecker.compareTo(ourNumber) < 0;
-                        primeChecker = primeChecker.add(BigInteger.ONE)){
-                                if(BigInteger.ZERO.equals(ourNumber.mod(primeChecker))){
-                                        isPrime = false;
-                                        break;
+                while(number != 1){
+                        newNumber = number;
+                        for(int index = 0; index < prime.length; index++){
+				if(prime[index] == 0){
+					break;
+				}
+                                if((number % prime[index]) == 0){
+                                        listOfPrimes.push(prime[index]);
+                                        number /= prime[index];
+                                        index = 0;
                                 }
                         }
-                        if(isPrime){
-                                System.out.println(ourNumber + " is prime");
-                                foundNumbers++;
-                                System.out.println("Found " + foundNumbers);
+			if(number == 1){
+				break;
+			}
+                        if(newNumber == number){
+                                System.out.println("Sorry, the next prime for your number can't be found, try a different number.");
+                                System.exit(1);
                         }
-                        ourNumber = ourNumber.nextProbablePrime();
                 }
+
+                size = listOfPrimes.getSize();
+                int[] numbers = new int[size];
+
+                while(!listOfPrimes.empty()){
+                        for(int i = 0; i < size; i++){
+                                numbers[i] = listOfPrimes.pop();
+                        }
+                }
+                Arrays.sort(numbers);
+
+                System.out.print("We found the following prime numbers: ");
+                for(int i = size - 1; i > -1; i--){
+                        System.out.print(numbers[i] + " ");
+                }
+                System.out.println();
+        }
+
+        public static int[] getPrimes(int maxNumber, int[] prime){
+
+		int counter = 0;
+		boolean isPrime = true;
+		int currentNumber = 2;
+		int i = 2;
+		boolean exists = false;
+		int j = 0;
+
+		while(currentNumber < maxNumber){
+			isPrime = true;
+			i = 2;
+			j = 0;
+			exists = false;
+			for(i = 2; i < currentNumber; i++){
+				if(currentNumber % i == 0){
+					isPrime = false;
+					break;
+				}
+			}
+			if(isPrime){
+				if(counter == 100){
+					currentNumber = maxNumber;
+					break;
+				}
+				for(j = 0; j <= counter; j ++){
+					if(prime[j] == currentNumber){
+						exists = true;
+						break;
+					}
+				}
+				if(!exists){
+					prime[counter] = i;
+					counter++;
+				}
+			}
+			currentNumber++;
+		}
+		return prime;
         }
 }
-
